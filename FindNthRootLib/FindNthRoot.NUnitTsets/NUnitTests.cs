a4c01f481460e50c5ead38c036deea819d703185
Elijah <gemar19@mail.ru>
2018-03-21 03:54:45 +0300

    Newton's method

---
+++ FindNthRootLib/FindNthRoot.NUnitTsets/NUnitTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNthRoot.NUnitTsets
+{
+    [TestFixture]
+    public class NUnitTests
+    {
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+
+
+        public void NewtonMethod(double number, int degree, double precision, double result)
+        {
+            Assert.AreEqual(FindNthRootLib.FindNthRootClassLib.FindNthRoot(number, degree, precision), result, precision);
+        }
+
+        [TestCase(8, 15, -7, -5)]
+        [TestCase(8, 15, -0.6, -0.1)]
+
+        public void FindNthRoot_Number_Degree_Precision_ArgumentOutOfRangeException(int number, int degree, double precision, double expected)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => FindNthRootLib.FindNthRootClassLib.FindNthRoot(number, degree, precision));
+        }
+
+
+    }
+}
---
+++ FindNthRootLib/FindNthRootLib/FindNthRootClassLib.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace FindNthRootLib
+{
+    public class FindNthRootClassLib
+    {
+
+        /// <summary>
+        ///  Newton's algorithm for getting n-th root of number.
+        /// </summary>
+        /// <param name="number">A number to get root</param>
+        /// <param name="degree">Degree</param>
+        /// <param name="accuracy"></param>
+        /// <returns>N-th root of 'number'</returns>
+        public static double FindNthRoot(double number, int degree, double accuracy)
+        {
+
+            if (degree < 0 || degree < int.MinValue || degree > int.MaxValue || accuracy < 0)
+            {
+                throw new ArgumentOutOfRangeException($"Argument's {nameof(degree)} isn't valid");
+            }
+
+            if (degree == 0)
+            {
+                return 1;
+            }
+            else if (degree == 1)
+            {
+                return number;
+            }
+
+
+            var x0 = number / degree;
+            var x1 = (1.0 / degree) * ((degree - 1) * x0 + number / Math.Pow(x0, degree - 1));
+
+            while (Math.Abs(x1 - x0) > accuracy)
+            {
+                x0 = x1;
+                x1 = (1.0 / degree) * ((degree - 1) * x0 + number / Math.Pow(x0, degree - 1));
+
+            }
+
+            return x1;
+        }
+    }
+}
